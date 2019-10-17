3efb91c7596a333d39d425d6fc5334d5f9bed42a
imamsatrianta <imamsatrianta@gmail.com>
2019-10-17 17:46:07 +0700

    update database

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(function(req, res, next) {
 });
 
 const sequelize = new Sequelize(process.env.DBNAME, process.env.DBUSER, process.env.DBPASSWORD, {
-  host: 'localhost',
+  host: 'sql12.freemysqlhosting.net',
   dialect: 'mysql',
   // operatorsAliases: false,
   pool: {
