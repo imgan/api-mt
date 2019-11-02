1de2f2d68d9594bce1a1f5b724925be6e2c1aed6
imamsatrianta <47249135+imgan@users.noreply.github.com>
2019-11-02 10:46:18 +0700

    update koneksi

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ app.use(function(req, res, next) {
 });
 
 const sequelize = new Sequelize(process.env.DBNAME, process.env.DBUSER, process.env.DBPASSWORD, {
-  host: 'localhost',
+  host: '192.168.1.5',
   dialect: 'mysql',
   // operatorsAliases: false,
   pool: {
